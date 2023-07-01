a7129194148a3c4933c1937a1a40992bcd693c83
Krishna47ak <anandakrishna720@gmail.com>
2023-07-01 11:40:43 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const connectDB = require('./config/db')
+
+const app = express()
+app.use(cors());
+
+
+// Connect Database
+connectDB()
+
+// Init middlewares
+app.use(express.json({ extended: false }))
+
+app.get('/', (req, res) => res.send('Api Running'))
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => console.log(`Server started at ${PORT} `))
+
+
