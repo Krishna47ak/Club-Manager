7eab545502323b5c8b1a7082103eaed7a92237ed
Shuklashashi0706 <shuklashashi169@gmail.com>
2023-07-01 21:55:09 +0530

    Navbar added

--- Frontend/club-manager/src/App.js
+++ Frontend/club-manager/src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
+import "./App.css";
 
-import Home from './screens/Home';
+import Home from "./screens/Home";
+import Navbar from "./components/Navbar/Navbar.js";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import store from "./store/store";
 
 function App() {
   return (
-    <Provider store={store} >
+    <Provider store={store}>
       <Router>
+        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
         </Routes>
