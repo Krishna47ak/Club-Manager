7a8dcae36f024582b2d7dd10616f9cb3a922eba8
Shuklashashi0706 <shuklashashi169@gmail.com>
2023-07-01 21:56:57 +0530

    Footer added

--- Frontend/club-manager/src/App.js
+++ Frontend/club-manager/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 
 import Home from "./screens/Home";
 import Navbar from "./components/Navbar/Navbar.js";
+import Footer from "./components/Footer/footer.js";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
         </Routes>
+        <Footer/>
       </Router>
     </Provider>
   );
